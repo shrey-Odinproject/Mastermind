1a0eefd33403a4fa41aa5e4871d072b3a66b268c
shrey-Odinproject <sonishreyf1@gmail.com>
2021-12-10 01:14:11 +0530

    created a working game but the hints are buggy and play method is far too complex , entire code need restructuring

--- mastermind.rb
+++ mastermind.rb
@@ -1,20 +1,21 @@
 # module for displaying stuff
+require 'pry-byebug'
 module Display
   def display_intro
-    'well play mastermind'
+    puts'well play mastermind'
   end
 
   def display_ask_input
-    'enter a 4 digit number with digits ranging from 1-6'
+    puts'enter a 4 digit number with digits ranging from 1-6'
   end
 
   def display_chances(chance)
-    puts "U have #{chance} remaining"
+    puts "U have #{chance} chances remaining"
     '----------'
   end
 
   def display_input_error_msg
-    'u can only enter 4 digit number with digits ranging from 1-6 ...'
+    puts'u can only enter 4 digit number with digits ranging from 1-6 ...'
   end
 
   def display_hint_info
@@ -38,7 +39,7 @@ class Comp
 end
 
 class Player
-  attr_reader :guess
+  attr_accessor :guess
   
   def initialize
     @guess=[]
@@ -56,20 +57,21 @@ class Player
 end
 
 class GameLogic
-  attr_reader :num_of_guesses, :hints
+  attr_accessor :num_of_guesses, :hints
   include Display
   
   def initialize
-    @num_of_guesses=12
+    @num_of_guesses=3
     @hints=[]
   end
 
-  def won?(comp,player)
+  def win?(comp,player)
     return true if comp.secret_code==player.guess
     false
   end
 
   def give_hint(comp,player)
+    @hints=[]
     player.guess.each_with_index do |elm,idx|
       if comp.secret_code.include?(elm)
         if comp.secret_code[idx]==player.guess[idx]
@@ -82,15 +84,52 @@ class GameLogic
     hints.shuffle
   end
   
-  # 1 round of mastermind
-  def play(comp,player)
-    while num_of_guesses!=0 || won?
-      
+  # It would be truthy if string contains only digits or if it is an empty string. Otherwise returns false.
+  def check_string(string)
+    string.scan(/\D/).empty?
+  end
+  
+  def get_input(player)
+    while true
+      display_ask_input
+      input=gets.chomp
+      if input.length!=4 || !check_string(input)
+        display_input_error_msg
+      else
+        player.make_guess(input.to_i)
+        break
+      end
     end
   end
-
+  # 1 round of mastermind
+  def play
+    display_intro
+    display_hint_info
+    display_chances(num_of_guesses)
+    player=Player.new
+    comp=Comp.new
+    comp.generate_code
+    #puts "ans was #{comp.secret_code}"
+    #binding.pry
+    while num_of_guesses!=0 || !win?
+      get_input(player)
+      if win?(comp,player)
+        puts 'u won'
+        break
+      end
+      self.num_of_guesses-=1
+      p give_hint(comp,player)
+      player.guess=[] # player guess needs to be set to empty else on 2nd guess it becomes like [1,2,3,2,4,5,3,1] i.e has 2 guesses in 1
+      display_chances(num_of_guesses)
+      if num_of_guesses==0
+        puts "ans was #{comp.secret_code}"
+        break
+      end
+    end
+    puts 'game over'
+  end
 end
-
+=begin
 p1=Player.new
 c1=Comp.new
 g1=GameLogic.new
@@ -99,6 +138,9 @@ c1.generate_code
 p1.make_guess(4136)
 
 print p1.guess; print c1.secret_code
-puts g1.won?(c1,p1)
+puts g1.win?(c1,p1)
 
 print g1.give_hint(c1,p1)
+=end
+game=GameLogic.new
+game.play
