43b5d24af22d78f4b4121a956ac99c84f56a195d
shrey-Odinproject <sonishreyf1@gmail.com>
2021-12-10 14:19:30 +0530

    improved hints function with minimum bugs although give_hints is a bit complex, as we give Xs and Os seprately

--- mastermind.rb
+++ mastermind.rb
@@ -1,21 +1,22 @@
-# module for displaying stuff
 require 'pry-byebug'
+
+# module for displaying stuff
 module Display
   def display_intro
-    puts'well play mastermind'
+    puts 'We are playing mastermind'
   end
 
   def display_ask_input
-    puts'enter a 4 digit number with digits ranging from 1-6'
+    puts 'enter a 4 digit number with digits ranging from 1-6'
   end
 
   def display_chances(chance)
     puts "U have #{chance} chances remaining"
-    '----------'
+    puts '----------'
   end
 
   def display_input_error_msg
-    puts'u can only enter 4 digit number with digits ranging from 1-6 ...'
+    puts 'u can only enter 4 digit number with digits ranging from 1-6 ...'
   end
 
   def display_hint_info
@@ -24,76 +25,94 @@ module Display
   end
 end
 
+# comp class that holds secret code
 class Comp
   attr_reader :secret_code
-  
+
   def initialize
-    @secret_code=[]
+    @secret_code = []
   end
-  
+
   def generate_code
-    while secret_code.length<4
-      secret_code.push(rand(1..6))
-    end
+    secret_code.push(rand(1..6)) while secret_code.length < 4
   end
 end
 
+# the player of game
 class Player
   attr_accessor :guess
-  
+
   def initialize
-    @guess=[]
+    @guess = []
   end
 
   # player will type a number and it will be passed to this method
-  def make_guess(num) 
-    while num!=0 do 
-      a=num%10
+  def make_guess(num)
+    # 1256 is converted to [1,2,5,6]
+    while num != 0
+      a = num % 10
       guess.push(a)
-      num=num/10
+      num /= 10
     end
     guess.reverse!
   end
 end
 
+# class to dictate game flow and stores all logic
 class GameLogic
   attr_accessor :num_of_guesses, :hints
+
   include Display
-  
+
   def initialize
-    @num_of_guesses=3
-    @hints=[]
+    @num_of_guesses = 12
+    @hints = []
   end
 
-  def win?(comp,player)
-    return true if comp.secret_code==player.guess
+  def win?(comp, player)
+    return true if comp.secret_code == player.guess
+
     false
   end
 
-  def give_hint(comp,player)
-    @hints=[]
-    player.guess.each_with_index do |elm,idx|
-      if comp.secret_code.include?(elm)
-        if comp.secret_code[idx]==player.guess[idx]
-          hints.push("X")
-        else
-          hints.push("O")
+  # gives Xs and Os according to guess
+  def give_hint(comp, player)
+    @hints = []
+    player_mod = player.guess.clone # so we dont modify our guess mid way of programm
+    comp_mod = comp.secret_code.clone # we use clone because we cant modify original secret code
+    repeat = true # determines if we repeat 'each' loop from idx 0
+    while repeat # whenever X is found array are changed and each loop is reset and run again from idx =0
+      repeat = false
+      player_mod.each_with_index do |_elm, idx|
+        if player_mod[idx] == comp_mod[idx] # we check for Xs and remove both same elements from both arrays
+          # eg [1,1,0,3]sec [3,1,4,1]guess so they become [1,0,3] & [3,4,1] and hints=[X]
+          hints.push('X')
+          player_mod.delete_at(idx)
+          comp_mod.delete_at(idx)
+          repeat = true
+          break
         end
       end
     end
-    hints.shuffle
+
+    player_mod.each do |elm| # now to find o we loop over [3,4,1] & [1,0,3] and not the original this enures correct num of Os
+      hints.push('O') if comp_mod.include?(elm)
+    end
+    player.guess = [] # player guess needs to be reset else on 2nd guess it becomes like [1,2,3,2,4,5,3,1] i.e has 2 guesses in 1
+    hints.shuffle # so u dont know which num are we talking about
   end
-  
+
   # It would be truthy if string contains only digits or if it is an empty string. Otherwise returns false.
   def check_string(string)
     string.scan(/\D/).empty?
   end
-  
+
+  # checks and gets input from user
   def get_input(player)
     while true
       display_ask_input
-      input=gets.chomp
-      if input.length!=4 || !check_string(input)
+      input = gets.chomp
+      if input.length != 4 || !check_string(input) || input.include?('0')
         display_input_error_msg
       else
         player.make_guess(input.to_i)
@@ -101,27 +120,27 @@ class GameLogic
       end
     end
   end
+
   # 1 round of mastermind
   def play
     display_intro
     display_hint_info
     display_chances(num_of_guesses)
-    player=Player.new
-    comp=Comp.new
+    player = Player.new
+    comp = Comp.new
     comp.generate_code
-    #puts "ans was #{comp.secret_code}"
-    #binding.pry
-    while num_of_guesses!=0 || !win?
+    # puts "ans was #{comp.secret_code}"
+    # binding.pry
+    while num_of_guesses != 0 || !win?
       get_input(player)
-      if win?(comp,player)
-        puts 'u won'
+      if win?(comp, player) # u win
+        puts 'Victory!! '
         break
       end
-      self.num_of_guesses-=1
-      p give_hint(comp,player)
-      player.guess=[] # player guess needs to be set to empty else on 2nd guess it becomes like [1,2,3,2,4,5,3,1] i.e has 2 guesses in 1
+      self.num_of_guesses -= 1
+      puts "hint: #{give_hint(comp, player).join}"
       display_chances(num_of_guesses)
-      if num_of_guesses==0
+      if num_of_guesses == 0 # u lost
         puts "ans was #{comp.secret_code}"
         break
       end
@@ -129,18 +148,6 @@ class GameLogic
     puts 'game over'
   end
 end
-=begin
-p1=Player.new
-c1=Comp.new
-g1=GameLogic.new
 
-c1.generate_code
-p1.make_guess(4136)
-
-print p1.guess; print c1.secret_code
-puts g1.win?(c1,p1)
-
-print g1.give_hint(c1,p1)
-=end
-game=GameLogic.new
+game = GameLogic.new
 game.play
